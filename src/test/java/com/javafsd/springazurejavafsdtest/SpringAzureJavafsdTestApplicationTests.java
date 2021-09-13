2bad300cf684bfae1c5923a3f2df88620e58479f
diksha <dikshamesh0895@gmail.com>
2021-09-13 15:12:37 +0530

    first commit

---
+++ src/test/java/com/javafsd/springazurejavafsdtest/SpringAzureJavafsdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javafsd.springazurejavafsdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureJavafsdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
